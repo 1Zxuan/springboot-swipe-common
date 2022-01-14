336fe1b540bfc200797374ea6eff487a263d3e45
64390357@qq.com <64390357@qq.com>
2022-01-14 10:38:04 +0800

    新增任务状态

---
+++ core/src/main/java/com/louzx/swipe/core/constants/TaskStatus.java
@@ -0,0 +1,12 @@
+package com.louzx.swipe.core.constants;
+
+public interface TaskStatus {
+    public static final int ERROR_TASK = -1;
+    public static final int TASK = 0;
+    public static final int RUN = 1;
+    public static final int FINISH = 3;
+    public static final int READ = 5;
+    public static final int STOP = 100;
+    public static final int STOP_READ = 101;
+    public static final int STOP_FINISH = 102;
+}
