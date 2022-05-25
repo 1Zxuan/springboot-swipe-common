3de4b8396bf70a3f9f2713d4bc8f772cd2f74a53
louzx <12345678>
2022-05-25 08:14:23 +0800

    启用异步更新

--- core/src/main/java/com/louzx/swipe/core/service/IUpdateService.java
+++ core/src/main/java/com/louzx/swipe/core/service/IUpdateService.java
@@ -2,6 +2,10 @@ package com.louzx.swipe.core.service;
 
 import com.louzx.swipe.core.jdbc.SqlBuilder;
 
+/**
+ * @description 异步更新数据库导致数据库异常
+ * */
+@Deprecated
 public interface IUpdateService {
 
     void update(SqlBuilder sqlBuilder);
