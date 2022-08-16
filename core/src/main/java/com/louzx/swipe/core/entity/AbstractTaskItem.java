08e448a62a3bc50300af2afa12c17ba71ea34c70
louzx <64390357@qq.com>
2022-08-16 17:42:18 +0800

    调整抽象类

--- core/src/main/java/com/louzx/swipe/core/entity/AbstractTaskItem.java
+++ core/src/main/java/com/louzx/swipe/core/entity/AbstractTaskItem.java
@@ -3,13 +3,17 @@ package com.louzx.swipe.core.entity;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Getter
 @Setter
 public abstract class AbstractTaskItem {
 
     protected String goodsName;
+    protected AtomicInteger goodsStock;
 
     public abstract String getGoodsId();
     public abstract Integer getNum();
     public abstract String getId();
+    public abstract void setNum(Integer num);
 }
