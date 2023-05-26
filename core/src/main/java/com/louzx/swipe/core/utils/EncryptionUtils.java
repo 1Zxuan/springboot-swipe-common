1fc22218b85b1c7f7467e3415f182dda596cd7bc
7234 <7234>
2023-05-26 08:55:07 +0800

    增加base64加解密方法

--- core/src/main/java/com/louzx/swipe/core/utils/EncryptionUtils.java
+++ core/src/main/java/com/louzx/swipe/core/utils/EncryptionUtils.java
@@ -3,6 +3,7 @@ package com.louzx.swipe.core.utils;
 import org.springframework.util.DigestUtils;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 public class EncryptionUtils {
 
@@ -10,4 +11,11 @@ public class EncryptionUtils {
         return DigestUtils.md5DigestAsHex(plaintext.getBytes(StandardCharsets.UTF_8));
     }
 
+    public static String base64 (String text) {
+        return Base64.getEncoder().encodeToString(text.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public static String unBase64 (String text) {
+        return new String(Base64.getDecoder().decode(text.getBytes(StandardCharsets.UTF_8)));
+    }
 }
