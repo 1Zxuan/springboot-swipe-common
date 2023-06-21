84dcaf4dd115abc73d6b2182f00fd028f30c269c
7234 <7234>
2023-06-21 09:00:10 +0800

    增加立即发送接口

--- core/src/main/java/com/louzx/swipe/core/service/INotifyService.java
+++ core/src/main/java/com/louzx/swipe/core/service/INotifyService.java
@@ -3,4 +3,6 @@ package com.louzx.swipe.core.service;
 public interface INotifyService {
 
     int notify(String title, String content, String uIds);
+
+    boolean notifyNow(String title, String content, String uIds);
 }
--- core/src/main/java/com/louzx/swipe/core/service/impl/NotifyServiceImpl.java
+++ core/src/main/java/com/louzx/swipe/core/service/impl/NotifyServiceImpl.java
@@ -1,7 +1,11 @@
 package com.louzx.swipe.core.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.louzx.swipe.core.constants.CommonConstants;
+import com.louzx.swipe.core.constants.Method;
 import com.louzx.swipe.core.service.INotifyService;
+import com.louzx.swipe.core.utils.HttpClientUtils;
+import com.louzx.swipe.core.utils.SwipeUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -9,14 +13,19 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class NotifyServiceImpl implements INotifyService {
 
     private final JdbcTemplate jdbcTemplate;
+    private final Map<String, String> header = new HashMap<String, String>() {{
+        put("Content-Type", "application/json");
+        put("Host", "wxpusher.zjiecode.com");
+        put("Accept", "*/*");
+        put("Accept-Encoding", "gzip, deflate, br");
+        put("Connection", "close");
+    }};
 
     @Autowired(required = false)
     public NotifyServiceImpl(@Qualifier("jdbcTemplateWx") JdbcTemplate jdbcTemplate) {
@@ -37,5 +46,19 @@ public class NotifyServiceImpl implements INotifyService {
         }
 
         return 0;
+    }
+
+    @Override
+    public boolean notifyNow(String title, String content, String uIds) {
+        JSONObject body = new JSONObject();
+        body.put("appToken", "AT_1Yx3TUzemrfzpR2zyomSrd2eoXPqouDK");
+        body.put("contentType", "2");
+        body.put("topicIds", Collections.singleton("3006"));
+        body.put("content", content);
+        body.put("summary", title);
+        body.put("uids", uIds.split(","));
+        String response = HttpClientUtils.doHttp("https://wxpusher.zjiecode.com/api/send/message", Method.POST, header, body.toString());
+        JSONObject jsonObject = SwipeUtils.parseJson(response);
+        return null != jsonObject && jsonObject.getBooleanValue("success");
     }
 }
